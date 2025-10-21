8aa045725223868558f43f099516b53f76add6d6
MUKILTEO\1722453 <davewilson260@gmail.com>
2025-10-21 12:08:45 -0700

    added movement script

---
+++ Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,49 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TopDownMovement : MonoBehaviour
+{
+    public float walkspeed = 5f;
+    public float runspeed = 10f;
+
+    private float currentSpeed;
+    private Vector2 movement;
+    private Rigidbody2D rb2D;
+
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        currentSpeed = walkspeed;
+    }
+
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rb2D.linearVelocity = movement * currentSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        movement = ctx.ReadValue<Vector2>();
+    }
+
+    public void Run(InputAction.CallbackContext ctx)
+    {
+        if (ctx.ReadValue<float>() == 1) //pressed
+        {
+            currentSpeed = runspeed;
+        }
+        else //released
+        {
+            currentSpeed = walkspeed;
+        }
+    }
+
+}
+
